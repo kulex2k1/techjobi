b507d1b5044c9707cd9acc9f3edccae07720f361
kulex2k1 <17738017+kulex2k1@users.noreply.github.com>
2021-06-13 01:25:05 +0100

    Add files via upload

--- Itjobs/settings.py
+++ Itjobs/settings.py
@@ -140,12 +140,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = [os.path.join(BASE_DIR, 'staticfiles')]
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 # MEDIA_URL = 'F:/CCB Files/portal/static/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
