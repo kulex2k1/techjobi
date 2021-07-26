54c41c12574b2d9dc22c70fd63c4f533437cb516
kulex2k1 <17738017+kulex2k1@users.noreply.github.com>
2021-07-26 11:30:03 +0100

    Update settings.py

--- Itjobs/settings.py
+++ Itjobs/settings.py
@@ -103,6 +103,19 @@ DATABASES = {
 }
 
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd5vnd6t4hlf07c', 
+        'USER': 'eugeosntimheia', 
+        'PASSWORD': 'b49ff148c64d148a6142e833c9538369f8c08011f30dafe1beffa7b2049516f9',
+        'HOST': 'ec2-3-224-7-166.compute-1.amazonaws.com', 
+        'PORT': '5432',
+    }
+}
+
+
+
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
